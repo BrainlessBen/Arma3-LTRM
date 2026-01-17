5a481dd33a2fa8e263ab3f85a19b357e22c8afe6
Ben van Loon <Bennie_vloon@outlook.com>
2026-01-17 09:48:53 +0100

    Import project

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Arma_3_LTRM
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
